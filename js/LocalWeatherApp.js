1ce3ed3c0bde69716840e991faadf3096f0b8682
Atul Sharma <atulsharma3108@gmail.com>
2017-05-02 14:37:43 +0900

    Update LocalWeatherApp.js
    Improved Code

--- js/LocalWeatherApp.js
+++ js/LocalWeatherApp.js
@@ -1,457 +1,478 @@
-var weatherIcons = {
-  "200": {
-    "label": "thunderstorm with light rain",
-    "icon": "storm-showers"
-  },
-
-  "201": {
-    "label": "thunderstorm with rain",
-    "icon": "storm-showers"
-  },
-
-  "202": {
-    "label": "thunderstorm with heavy rain",
-    "icon": "storm-showers"
-  },
-
-  "210": {
-    "label": "light thunderstorm",
-    "icon": "storm-showers"
-  },
-
-  "211": {
-    "label": "thunderstorm",
-    "icon": "thunderstorm"
-  },
-
-  "212": {
-    "label": "heavy thunderstorm",
-    "icon": "thunderstorm"
-  },
-
-  "221": {
-    "label": "ragged thunderstorm",
-    "icon": "thunderstorm"
-  },
-
-  "230": {
-    "label": "thunderstorm with light drizzle",
-    "icon": "storm-showers"
-  },
-
-  "231": {
-    "label": "thunderstorm with drizzle",
-    "icon": "storm-showers"
-  },
-
-  "232": {
-    "label": "thunderstorm with heavy drizzle",
-    "icon": "storm-showers"
-  },
-
-  "300": {
-    "label": "light intensity drizzle",
-    "icon": "sprinkle"
-  },
-
-  "301": {
-    "label": "drizzle",
-    "icon": "sprinkle"
-  },
-
-  "302": {
-    "label": "heavy intensity drizzle",
-    "icon": "sprinkle"
-  },
-
-  "310": {
-    "label": "light intensity drizzle rain",
-    "icon": "sprinkle"
-  },
-
-  "311": {
-    "label": "drizzle rain",
-    "icon": "sprinkle"
-  },
-
-  "312": {
-    "label": "heavy intensity drizzle rain",
-    "icon": "sprinkle"
-  },
-
-  "313": {
-    "label": "shower rain and drizzle",
-    "icon": "sprinkle"
-  },
-
-  "314": {
-    "label": "heavy shower rain and drizzle",
-    "icon": "sprinkle"
-  },
-
-  "321": {
-    "label": "shower drizzle",
-    "icon": "sprinkle"
-  },
-
-  "500": {
-    "label": "light rain",
-    "icon": "rain"
-  },
-
-  "501": {
-    "label": "moderate rain",
-    "icon": "rain"
-  },
-
-  "502": {
-    "label": "heavy intensity rain",
-    "icon": "rain"
-  },
-
-  "503": {
-    "label": "very heavy rain",
-    "icon": "rain"
-  },
-
-  "504": {
-    "label": "extreme rain",
-    "icon": "rain"
-  },
-
-  "511": {
-    "label": "freezing rain",
-    "icon": "rain-mix"
-  },
-
-  "520": {
-    "label": "light intensity shower rain",
-    "icon": "showers"
-  },
-
-  "521": {
-    "label": "shower rain",
-    "icon": "showers"
-  },
-
-  "522": {
-    "label": "heavy intensity shower rain",
-    "icon": "showers"
-  },
-
-  "531": {
-    "label": "ragged shower rain",
-    "icon": "showers"
-  },
-
-  "600": {
-    "label": "light snow",
-    "icon": "snow"
-  },
-
-  "601": {
-    "label": "snow",
-    "icon": "snow"
-  },
-
-  "602": {
-    "label": "heavy snow",
-    "icon": "snow"
-  },
-
-  "611": {
-    "label": "sleet",
-    "icon": "sleet"
-  },
-
-  "612": {
-    "label": "shower sleet",
-    "icon": "sleet"
-  },
-
-  "615": {
-    "label": "light rain and snow",
-    "icon": "rain-mix"
-  },
-
-  "616": {
-    "label": "rain and snow",
-    "icon": "rain-mix"
-  },
-
-  "620": {
-    "label": "light shower snow",
-    "icon": "rain-mix"
-  },
-
-  "621": {
-    "label": "shower snow",
-    "icon": "rain-mix"
-  },
-
-  "622": {
-    "label": "heavy shower snow",
-    "icon": "rain-mix"
-  },
-
-  "701": {
-    "label": "mist",
-    "icon": "sprinkle"
-  },
-
-  "711": {
-    "label": "smoke",
-    "icon": "smoke"
-  },
-
-  "721": {
-    "label": "haze",
-    "icon": "day-haze"
-  },
-
-  "731": {
-    "label": "sand, dust whirls",
-    "icon": "cloudy-gusts"
-  },
-
-  "741": {
-    "label": "fog",
-    "icon": "fog"
-  },
-
-  "751": {
-    "label": "sand",
-    "icon": "cloudy-gusts"
-  },
-
-  "761": {
-    "label": "dust",
-    "icon": "dust"
-  },
-
-  "762": {
-    "label": "volcanic ash",
-    "icon": "smog"
-  },
-
-  "771": {
-    "label": "squalls",
-    "icon": "day-windy"
-  },
-
-  "781": {
-    "label": "tornado",
-    "icon": "tornado"
-  },
-
-  "800": {
-    "label": "clear sky",
-    "icon": "sunny"
-  },
-
-  "801": {
-    "label": "few clouds",
-    "icon": "cloudy"
-  },
-
-  "802": {
-    "label": "scattered clouds",
-    "icon": "cloudy"
-  },
-
-  "803": {
-    "label": "broken clouds",
-    "icon": "cloudy"
-  },
-
-  "804": {
-    "label": "overcast clouds",
-    "icon": "cloudy"
-  },
-
-  "900": {
-    "label": "tornado",
-    "icon": "tornado"
-  },
-
-  "901": {
-    "label": "tropical storm",
-    "icon": "hurricane"
-  },
-
-  "902": {
-    "label": "hurricane",
-    "icon": "hurricane"
-  },
-
-  "903": {
-    "label": "cold",
-    "icon": "snowflake-cold"
-  },
-
-  "904": {
-    "label": "hot",
-    "icon": "hot"
-  },
-
-  "905": {
-    "label": "windy",
-    "icon": "windy"
-  },
-
-  "906": {
-    "label": "hail",
-    "icon": "hail"
-  },
-
-  "951": {
-    "label": "calm",
-    "icon": "sunny"
-  },
-
-  "952": {
-    "label": "light breeze",
-    "icon": "cloudy-gusts"
-  },
-
-  "953": {
-    "label": "gentle breeze",
-    "icon": "cloudy-gusts"
-  },
-
-  "954": {
-    "label": "moderate breeze",
-    "icon": "cloudy-gusts"
-  },
-
-  "955": {
-    "label": "fresh breeze",
-    "icon": "cloudy-gusts"
-  },
-
-  "956": {
-    "label": "strong breeze",
-    "icon": "cloudy-gusts"
-  },
-
-  "957": {
-    "label": "high wind, near gale",
-    "icon": "cloudy-gusts"
-  },
-
-  "958": {
-    "label": "gale",
-    "icon": "cloudy-gusts"
-  },
-
-  "959": {
-    "label": "severe gale",
-    "icon": "cloudy-gusts"
-  },
-
-  "960": {
-    "label": "storm",
-    "icon": "thunderstorm"
-  },
-
-  "961": {
-    "label": "violent storm",
-    "icon": "thunderstorm"
-  },
-
-  "962": {
-    "label": "hurricane",
-    "icon": "cloudy-gusts"
-  }
-}
-var DEG = 'c';
-$(document).ready(function() {
-  //console.log(new Date().getHours()) 
-  setbackground();
-
-  $('#degreeButton').click(function() {
-    var currentClass = $(this).children("i").attr("class");
-    var tempCurrent = $('#tempDegree').html();
-    if (currentClass === "wi wi-celsius") {
-      var fTemp = tempCurrent * 9 / 5 + 32;
-      $('#tempDegree').html(Math.round(fTemp))
-      $(this).children("i").attr('class', 'wi wi-fahrenheit');
-    } else {
-      var cTemp = (tempCurrent - 32) * 5 / 9;
-      $('#tempDegree').html(Math.round(cTemp));
-      $(this).children("i").attr('class', 'wi wi-celsius');
-    }
-  });
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latData = position.coords.latitude;
-      var lonData = position.coords.longitude;
-      $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latData + "&lon=" + lonData + "&appid=0b8fbf901847d3f390785a52bd76977e", function(json) {
-        $('#cityName').html(json.name + "," + json.sys.country);
-        var tempCelcius = parseInt(json.main.temp) - 273;
-        $('#tempDegree').html(Math.round(tempCelcius));
-        $('#weatherCondition').html(json.weather[0].main);
-
-        $('#pressureContent').html(json.main.pressure);
-        $('#humidityContent').html(json.main.humidity);
-        $('#windContent').html(json.wind.speed);
-        $('#visiblityContent').html(json.visibility);
-        $('#minTempContent').html(parseInt(json.main.temp_min) - 273);
-        $('#maxTempContent').html(parseInt(json.main.temp_max) - 273);
-        var sunrise = new Date(json.sys.sunrise * 1000);
-        var sunset = new Date(json.sys.sunrise * 1000);
-        $('#sunriseContent').html(getTimeFromUTC(sunrise));
-        $('#sunsetContent').html(getTimeFromUTC(sunset));
-        /* $('#weatherImage').attr('src',"http://openweathermap.org/img/w/"+json.weather[0].icon+".png"); */
-        /* $('#weatherIcon').attr('alt',json.weather[0].main); */
-
-        var prefix = 'wi wi-';
-        var code = json.weather[0].id;
-        var icon = weatherIcons[code].icon;
-
-        // If we are not in the ranges mentioned above, add a day/night prefix.
-        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
-          icon = 'day-' + icon;
-        }
-
-        // Finally tack on the prefix.
-        icon = prefix + icon;
-        console.log(icon);
-        $('#weatherIcon').addClass(icon);
-      });
-    });
-  }
-
-});
-
-function getTimeFromUTC(date) {
-  var hours = date.getHours();
-  // Minutes part from the timestamp
-  var minutes = "0" + date.getMinutes();
-  // Seconds part from the timestamp
-  var seconds = "0" + date.getSeconds();
-
-  // Will display time in 10:30:23 format
-  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-  return formattedTime;
-}
-
-function setbackground() {
-  var index = new Date().getHours();
-  if (index > 5 && index <= 7)
-    ColorValue = "clearMorning";
-  else if (index > 7 && index <= 17)
-    ColorValue = "clearDay";
-  else if (index > 17 && index <= 19)
-    ColorValue = "clearMorning";
-  else if (index > 19 && index <= 23)
-    ColorValue = "clearNight";
-  else if (index >= 0 && index <= 5)
-    ColorValue = "clearNight";
-
-  $("#mainContainer").addClass(ColorValue);
-  $("#degreeButton").addClass(ColorValue);
-
-}
+(function(window, document, $, undefined) {
+
+	window.localWeatherApp = {};
+	localWeatherApp.weatherIcons = {
+		"200" : {
+			"label" : "thunderstorm with light rain",
+			"icon" : "storm-showers"
+		},
+
+		"201" : {
+			"label" : "thunderstorm with rain",
+			"icon" : "storm-showers"
+		},
+
+		"202" : {
+			"label" : "thunderstorm with heavy rain",
+			"icon" : "storm-showers"
+		},
+
+		"210" : {
+			"label" : "light thunderstorm",
+			"icon" : "storm-showers"
+		},
+
+		"211" : {
+			"label" : "thunderstorm",
+			"icon" : "thunderstorm"
+		},
+
+		"212" : {
+			"label" : "heavy thunderstorm",
+			"icon" : "thunderstorm"
+		},
+
+		"221" : {
+			"label" : "ragged thunderstorm",
+			"icon" : "thunderstorm"
+		},
+
+		"230" : {
+			"label" : "thunderstorm with light drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"231" : {
+			"label" : "thunderstorm with drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"232" : {
+			"label" : "thunderstorm with heavy drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"300" : {
+			"label" : "light intensity drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"301" : {
+			"label" : "drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"302" : {
+			"label" : "heavy intensity drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"310" : {
+			"label" : "light intensity drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"311" : {
+			"label" : "drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"312" : {
+			"label" : "heavy intensity drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"313" : {
+			"label" : "shower rain and drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"314" : {
+			"label" : "heavy shower rain and drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"321" : {
+			"label" : "shower drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"500" : {
+			"label" : "light rain",
+			"icon" : "rain"
+		},
+
+		"501" : {
+			"label" : "moderate rain",
+			"icon" : "rain"
+		},
+
+		"502" : {
+			"label" : "heavy intensity rain",
+			"icon" : "rain"
+		},
+
+		"503" : {
+			"label" : "very heavy rain",
+			"icon" : "rain"
+		},
+
+		"504" : {
+			"label" : "extreme rain",
+			"icon" : "rain"
+		},
+
+		"511" : {
+			"label" : "freezing rain",
+			"icon" : "rain-mix"
+		},
+
+		"520" : {
+			"label" : "light intensity shower rain",
+			"icon" : "showers"
+		},
+
+		"521" : {
+			"label" : "shower rain",
+			"icon" : "showers"
+		},
+
+		"522" : {
+			"label" : "heavy intensity shower rain",
+			"icon" : "showers"
+		},
+
+		"531" : {
+			"label" : "ragged shower rain",
+			"icon" : "showers"
+		},
+
+		"600" : {
+			"label" : "light snow",
+			"icon" : "snow"
+		},
+
+		"601" : {
+			"label" : "snow",
+			"icon" : "snow"
+		},
+
+		"602" : {
+			"label" : "heavy snow",
+			"icon" : "snow"
+		},
+
+		"611" : {
+			"label" : "sleet",
+			"icon" : "sleet"
+		},
+
+		"612" : {
+			"label" : "shower sleet",
+			"icon" : "sleet"
+		},
+
+		"615" : {
+			"label" : "light rain and snow",
+			"icon" : "rain-mix"
+		},
+
+		"616" : {
+			"label" : "rain and snow",
+			"icon" : "rain-mix"
+		},
+
+		"620" : {
+			"label" : "light shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"621" : {
+			"label" : "shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"622" : {
+			"label" : "heavy shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"701" : {
+			"label" : "mist",
+			"icon" : "sprinkle"
+		},
+
+		"711" : {
+			"label" : "smoke",
+			"icon" : "smoke"
+		},
+
+		"721" : {
+			"label" : "haze",
+			"icon" : "day-haze"
+		},
+
+		"731" : {
+			"label" : "sand, dust whirls",
+			"icon" : "cloudy-gusts"
+		},
+
+		"741" : {
+			"label" : "fog",
+			"icon" : "fog"
+		},
+
+		"751" : {
+			"label" : "sand",
+			"icon" : "cloudy-gusts"
+		},
+
+		"761" : {
+			"label" : "dust",
+			"icon" : "dust"
+		},
+
+		"762" : {
+			"label" : "volcanic ash",
+			"icon" : "smog"
+		},
+
+		"771" : {
+			"label" : "squalls",
+			"icon" : "day-windy"
+		},
+
+		"781" : {
+			"label" : "tornado",
+			"icon" : "tornado"
+		},
+
+		"800" : {
+			"label" : "clear sky",
+			"icon" : "sunny"
+		},
+
+		"801" : {
+			"label" : "few clouds",
+			"icon" : "cloudy"
+		},
+
+		"802" : {
+			"label" : "scattered clouds",
+			"icon" : "cloudy"
+		},
+
+		"803" : {
+			"label" : "broken clouds",
+			"icon" : "cloudy"
+		},
+
+		"804" : {
+			"label" : "overcast clouds",
+			"icon" : "cloudy"
+		},
+
+		"900" : {
+			"label" : "tornado",
+			"icon" : "tornado"
+		},
+
+		"901" : {
+			"label" : "tropical storm",
+			"icon" : "hurricane"
+		},
+
+		"902" : {
+			"label" : "hurricane",
+			"icon" : "hurricane"
+		},
+
+		"903" : {
+			"label" : "cold",
+			"icon" : "snowflake-cold"
+		},
+
+		"904" : {
+			"label" : "hot",
+			"icon" : "hot"
+		},
+
+		"905" : {
+			"label" : "windy",
+			"icon" : "windy"
+		},
+
+		"906" : {
+			"label" : "hail",
+			"icon" : "hail"
+		},
+
+		"951" : {
+			"label" : "calm",
+			"icon" : "sunny"
+		},
+
+		"952" : {
+			"label" : "light breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"953" : {
+			"label" : "gentle breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"954" : {
+			"label" : "moderate breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"955" : {
+			"label" : "fresh breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"956" : {
+			"label" : "strong breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"957" : {
+			"label" : "high wind, near gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"958" : {
+			"label" : "gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"959" : {
+			"label" : "severe gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"960" : {
+			"label" : "storm",
+			"icon" : "thunderstorm"
+		},
+
+		"961" : {
+			"label" : "violent storm",
+			"icon" : "thunderstorm"
+		},
+
+		"962" : {
+			"label" : "hurricane",
+			"icon" : "cloudy-gusts"
+		}
+	}
+	localWeatherApp.init = function() {
+
+		if (navigator.geolocation) {
+
+			navigator.geolocation.getCurrentPosition(function(position) {
+
+				var latData = position.coords.latitude;
+				var lonData = position.coords.longitude;
+				$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latData + "&lon=" + lonData + "&appid=0b8fbf901847d3f390785a52bd76977e", function(json) {
+					$('#cityName').html(json.name + "," + json.sys.country);
+					var tempCelcius = parseInt(json.main.temp) - 273;
+					$('#tempDegree').html(Math.round(tempCelcius));
+					$('#weatherCondition').html(json.weather[0].main);
+
+					$('#pressureContent').html(json.main.pressure);
+					$('#humidityContent').html(json.main.humidity);
+					$('#windContent').html(json.wind.speed);
+					$('#visiblityContent').html(json.visibility);
+					$('#minTempContent').html(parseInt(json.main.temp_min) - 273);
+					$('#maxTempContent').html(parseInt(json.main.temp_max) - 273);
+					var sunrise = new Date(json.sys.sunrise * 1000);
+					var sunset = new Date(json.sys.sunrise * 1000);
+					$('#sunriseContent').html(localWeatherApp.getTimeFromUTC(sunrise));
+					$('#sunsetContent').html(localWeatherApp.getTimeFromUTC(sunset));
+					/* $('#weatherImage').attr('src',"http://openweathermap.org/img/w/"+json.weather[0].icon+".png"); */
+					/* $('#weatherIcon').attr('alt',json.weather[0].main); */
+
+					var prefix = 'wi wi-';
+					var code = json.weather[0].id;
+					var icon = localWeatherApp.weatherIcons[code].icon;
+
+					// If we are not in the ranges mentioned above, add a
+					// day/night prefix.
+					if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+						icon = 'day-' + icon;
+					}
+
+					// Finally tack on the prefix.
+					icon = prefix + icon;
+
+					$('#weatherIcon').addClass(icon);
+				});
+				localWeatherApp.setbackground();
+			});
+		}
+
+	}
+
+	localWeatherApp.degreeButtonClick = function() {
+		var currentClass = $(this).children("i").attr("class");
+		var tempCurrent = $('#tempDegree').html();
+		if (currentClass === "wi wi-celsius") {
+			var fTemp = tempCurrent * 9 / 5 + 32;
+			$('#tempDegree').html(Math.round(fTemp))
+			$(this).children("i").attr('class', 'wi wi-fahrenheit');
+		} else {
+			var cTemp = (tempCurrent - 32) * 5 / 9;
+			$('#tempDegree').html(Math.round(cTemp));
+			$(this).children("i").attr('class', 'wi wi-celsius');
+		}
+	}
+
+	localWeatherApp.getTimeFromUTC = function(date) {
+		var hours = date.getHours();
+		// Minutes part from the timestamp
+		var minutes = "0" + date.getMinutes();
+		// Seconds part from the timestamp
+		var seconds = "0" + date.getSeconds();
+
+		// Will display time in 10:30:23 format
+		var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+		return formattedTime;
+	}
+
+	localWeatherApp.setbackground = function() {
+		var index = new Date().getHours();
+		if (index > 5 && index <= 7)
+			ColorValue = "clearMorning";
+		else if (index > 7 && index <= 17)
+			ColorValue = "clearDay";
+		else if (index > 17 && index <= 19)
+			ColorValue = "clearMorning";
+		else if (index > 19 && index <= 23)
+			ColorValue = "clearNight";
+		else if (index >= 0 && index <= 5)
+			ColorValue = "clearNight";
+
+		$("#mainContainer").addClass(ColorValue);
+
+		$("#degreeButton").addClass(ColorValue);
+
+	}
+
+	localWeatherApp.init();
+
+	$("#degreeButton").on('click', localWeatherApp.degreeButtonClick);
+	
+	$(function() {
+
+		// The DOM is ready!
+		localWeatherApp.setbackground();
+		localWeatherApp.init();
+		
+		$("#degreeButton").on('click', localWeatherApp.degreeButtonClick);
+
+	});
+
+})(window, document, jQuery);
